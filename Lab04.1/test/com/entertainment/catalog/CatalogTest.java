96b895cd705ccb580c84faaabf2c88b267ad7763
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-05-17 15:17:54 -0700

    Lab04.1 (Catalog of Television) 2023-05-17 EOD

--- Lab04.1/test/com/entertainment/catalog/CatalogTest.java
+++ Lab04.1/test/com/entertainment/catalog/CatalogTest.java
@@ -10,18 +10,56 @@ package com.entertainment.catalog;
 
 import static org.junit.Assert.*;
 import java.util.Collection;
+import java.util.Map;
 import org.junit.Test;
 import com.entertainment.Television;
 
 public class CatalogTest {
-  
-  /**
-   * Contract: a no-matches result should be an empty collection (not null).
-   */
-  @Test
-  public void testFindByBrandNoMatches() {
-    Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
-    assertNotNull(tvs);
-    assertTrue(tvs.isEmpty());
-  }
+
+    @Test
+    public void findByBrands_shouldReturnPopulatedMap_whenBrandsPassed() {
+        Map<String,Collection<Television>> tvMap = Catalog.findByBrands("Sony", "NO-MATCHES");
+
+        assertEquals(2, tvMap.size());
+
+        // check the "Sony" row
+        Collection<Television> sonyTvs = tvMap.get("Sony");
+        assertEquals(7, sonyTvs.size());
+        for (Television tv : sonyTvs) {
+            assertEquals("Sony", tv.getBrand());
+        }
+
+        // check the "NO-MATCHES" row
+        Collection<Television> noMatchesTvs = tvMap.get("NO-MATCHES");
+        assertTrue(noMatchesTvs.isEmpty());
+    }
+
+    @Test
+    public void findByBrands_shouldReturnEmptyMap_whenNoBrandsPassed() {
+        Map<String,Collection<Television>> tvMap = Catalog.findByBrands();
+        // don't need both, Jay prefers isEmpty()
+        assertEquals(0, tvMap.size());
+        assertTrue(tvMap.isEmpty());
+    }
+
+    @Test
+    public void findByBrand_shouldReturnPopulatedCollection_whenMatchFound() {
+        Collection<Television> tvs = Catalog.findByBrand("Sony");
+
+        assertEquals(7, tvs.size());
+
+        for (Television tv : tvs) {
+            assertEquals("Sony", tv.getBrand());
+        }
+    }
+
+    /**
+     * Contract: a no-matches result should be an empty collection (not null).
+     */
+    @Test
+    public void findByBrand_shouldReturnEmptyCollection_whenNoMatchFound() {
+        Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
+        assertNotNull(tvs);
+        assertTrue(tvs.isEmpty());
+    }
 }
