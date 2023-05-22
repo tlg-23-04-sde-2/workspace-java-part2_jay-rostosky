6212a6490d7f21c95f2088624083b6ed8ab9bdc7
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-05-22 08:05:27 -0700

    Sorts via anonymous Comparator classes.

--- Scratch/src/com/crisp/RadishColorComparator.java
+++ Scratch/src/com/crisp/RadishColorComparator.java
@@ -6,6 +6,6 @@ class RadishColorComparator implements Comparator<Radish> {
 
     @Override
     public int compare(Radish r1, Radish r2) {
-        return r1.getColor().compareTo(r2.getColor());  // String is Comparable
+        return r1.getColor().compareTo(r2.getColor());
     }
 }
--- Scratch/src/com/crisp/RadishSortTest.java
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crisp;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -8,20 +9,41 @@ class RadishSortTest {
     public static void main(String[] args) {
         List<Radish> radishes = new ArrayList<>();
 
-        // color, size, tailLength, guysOnTop
+        //                       color,  size, tail, guys
         radishes.add(new Radish("red",    1.0,  2.2, 3));
         radishes.add(new Radish("black",  3.5,  0.0, 0));
         radishes.add(new Radish("red",    0.75, 3.1, 7));
         radishes.add(new Radish("white",  1.0,  1.0, 2));
 
+        System.out.println("Sort by natural order (size)");
         radishes.sort(null);  // passing null means natural order
-        System.out.println("Natural order (size)");
         dump(radishes);
         System.out.println();
 
+        System.out.println("Sort by color, via Comparator");
         radishes.sort(new RadishColorComparator());
-        System.out.println("Color");
         dump(radishes);
+        System.out.println();
+
+        System.out.println("Sort by tailLength, via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        });
+        dump(radishes);
+        System.out.println();
+
+        System.out.println("Sort by guysOnTop descending, via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r2.getGuysOnTop(), r1.getGuysOnTop());
+            }
+        });
+        dump(radishes);
+        System.out.println();
     }
 
     private static void dump(List<Radish> radishList) {
